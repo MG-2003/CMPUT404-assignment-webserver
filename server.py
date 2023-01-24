5b9739a3a235674caf4ce365baf40bf58ef2f8f9
MG-2003 <mgadhia@ualberta.ca>
2023-01-24 12:34:50 -0700

    Update server.py as per assignment specifications

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 #  coding: utf-8 
 import socketserver
+from os import path
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
 # 
@@ -28,11 +29,61 @@ import socketserver
 
 
 class MyWebServer(socketserver.BaseRequestHandler):
-    
+
+    def get_data(self, filePath):
+        with open(filePath, 'r') as file:
+            data = file.read()
+        return data
+
+    def send_error(self, code, message = '', requestPath = ''):
+        locationHeader = f"Location: http://127.0.0.1:8080{requestPath}/" if code == 301 else ""
+        error = f"HTTP/1.1 {code} {message}\r\n{locationHeader}"
+        self.request.sendall(bytearray(error,'utf-8'))
+
+    def send_data(self, fileType, filePath):
+        data = self.get_data(filePath)
+        msg = f"HTTP/1.1 200 OK\r\nContent-Type: text/{fileType}; charset=utf-8\r\n\r\n{data}"
+        self.request.sendall(bytearray(msg,'utf-8'))
+
+    def is_servable_dir(self, dirPath):
+        return self.is_servable_file(f"{dirPath}/")
+
+    def is_servable_file(self, filePath):
+        dirName = path.dirname(f"{filePath}").split("/")
+        return True if dirName[-1] == "www" or dirName[-2] == "www" else False
+
     def handle(self):
         self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+        # print ("Got a request of: %s\n" % self.data)
+
+        # Split incoming data by line
+        dataList = self.data.decode("utf-8").split('\r\n')
+        # Process request type ie. GET, POST, etc.
+        requestType = dataList[0].split(' ')[0]
+
+        if requestType != 'GET':
+            self.send_error(405, "Method Not Allowed")
+
+        else:
+            # Proccess request path
+            requestPath = dataList[0].split(' ')[1]
+            print('PATH:', path)
+
+            if requestPath[-1] != "/":
+                filePath = f"www{requestPath}"
+                if path.isfile(filePath) and self.is_servable_file(filePath):
+                    fileType = "css" if filePath[len(filePath)-3:] == "css" else "html"
+                    self.send_data(fileType, filePath)
+                elif path.isdir(filePath) and self.is_servable_dir(filePath):
+                    self.send_error(301, "Moved Permanently", requestPath)
+                else:
+                    self.send_error(404, "File Not Found")
+            else:
+                filePath = f"www{requestPath}/index.html"
+                if path.isfile(filePath) and self.is_servable_file(filePath):
+                    self.send_data("html", filePath)
+                else:
+                    self.send_error(404, "File Not Found")
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
