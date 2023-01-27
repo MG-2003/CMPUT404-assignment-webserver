e9e799bb6ab910972f276527615b419840136daf
MG-2003 <mgadhia@ualberta.ca>
2023-01-27 01:17:15 -0700

    update server.py and license

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 #  coding: utf-8 
 import socketserver
 from os import path
+from os import listdir
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
 # 
@@ -31,36 +32,50 @@ from os import path
 class MyWebServer(socketserver.BaseRequestHandler):
 
     def get_data(self, filePath):
+        # Get client-requested data
         with open(filePath, 'r') as file:
             data = file.read()
         return data
 
     def send_error(self, code, message = '', requestPath = ''):
+        # Send error back to client
         locationHeader = f"Location: http://127.0.0.1:8080{requestPath}/" if code == 301 else ""
         error = f"HTTP/1.1 {code} {message}\r\n{locationHeader}"
         self.request.sendall(bytearray(error,'utf-8'))
 
     def send_data(self, fileType, filePath):
+        # Send requested data back to client
         data = self.get_data(filePath)
         msg = f"HTTP/1.1 200 OK\r\nContent-Type: text/{fileType}; charset=utf-8\r\n\r\n{data}"
         self.request.sendall(bytearray(msg,'utf-8'))
+    
+    def get_servable_requests(self, filePath = "www"):
+        # List of all possible directories and files that can be served (are within www dir)
+        requests = []
+        for i in listdir(filePath):
+            requests.append(i)
+            if path.isdir(f"{filePath}/{i}"):
+                dirR = self.get_servable_requests(f"{filePath}/{i}")
+                for x in dirR:
+                    requests.append(x)
+        return requests
 
     def is_servable_dir(self, dirPath):
-        return self.is_servable_file(f"{dirPath}/")
+        return True if dirPath.split("/")[-1] in self.get_servable_requests() else False
 
     def is_servable_file(self, filePath):
-        dirName = path.dirname(f"{filePath}").split("/")
-        return True if dirName[-1] == "www" or dirName[-2] == "www" else False
+        return True if filePath.split("/")[-1] in self.get_servable_requests() else False
 
     def handle(self):
         self.data = self.request.recv(1024).strip()
-        # print ("Got a request of: %s\n" % self.data)
+        print ("Got a request of: %s\n" % self.data)
 
         # Split incoming data by line
         dataList = self.data.decode("utf-8").split('\r\n')
         # Process request type ie. GET, POST, etc.
         requestType = dataList[0].split(' ')[0]
 
+        # Send 405 if request isn't supported
         if requestType != 'GET':
             self.send_error(405, "Method Not Allowed")
 
@@ -69,22 +84,25 @@ class MyWebServer(socketserver.BaseRequestHandler):
             requestPath = dataList[0].split(' ')[1]
             print('PATH:', path)
 
+            # If request ends with slash, we assume they're requesting the index.html file in the requested dir
             if requestPath[-1] != "/":
                 filePath = f"www{requestPath}"
                 if path.isfile(filePath) and self.is_servable_file(filePath):
-                    fileType = "css" if filePath[len(filePath)-3:] == "css" else "html"
+                    #Grab the file extension (last element of the filePath, and information after the .) ex. /deep/base.css, would grab "css" frm base.css
+                    fileType = filePath.split("/")[-1].split(".")[-1]
                     self.send_data(fileType, filePath)
                 elif path.isdir(filePath) and self.is_servable_dir(filePath):
                     self.send_error(301, "Moved Permanently", requestPath)
                 else:
                     self.send_error(404, "File Not Found")
             else:
-                filePath = f"www{requestPath}/index.html"
+                filePath = f"www{requestPath}index.html"
+                print("FILEPATH", filePath)
                 if path.isfile(filePath) and self.is_servable_file(filePath):
                     self.send_data("html", filePath)
                 else:
                     self.send_error(404, "File Not Found")
-
+                    
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
 
